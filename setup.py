3e529a6495cc1bb29d53c92c7b41608138838618
Rasmus Andersen <recan@dtu.dk>
2024-02-12 16:03:53 +0000

    correctly install the simulation resources

--- setup.py
+++ setup.py
@@ -4,22 +4,31 @@ from setuptools import find_packages, setup
 
 package_name = 'my_turtlebot'
 
+data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml'])]
+
+def package_files(data_files, directory_list):
+    paths_dict = {}
+    for directory in directory_list:
+        for (path, directories, filenames) in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+                else:
+                    paths_dict[install_path] = [file_path]
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+    return data_files
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.xacro'))),
-        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world'))),
-        (os.path.join('share', package_name, 'maps'), glob(os.path.join('maps', '*.*'))),
-        (os.path.join('share', package_name, 'models'), [f for f in glob(os.path.join('models', '**/*'), recursive=True) if os.path.isfile(f)]),
-    ],
+    data_files=package_files(data_files, ['launch', 'rviz', 'urds', 'worlds', 'maps', 'models']),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
